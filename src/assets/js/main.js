8321b4eab44e5b2ab99b43b5843a38853354af2d
jozefrosenberger <jozef.rosenberger@superkoders.com>
2026-06-26 19:55:53 +0200

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,5 @@
 import { Application } from '@hotwired/stimulus';
 import HeaderController from './controllers/header';
-import DialogController from './controllers/dialog';
 
 window.Stimulus = Application.start();
 window.Stimulus.register('header', HeaderController);
-window.Stimulus.register('dialog', DialogController);
